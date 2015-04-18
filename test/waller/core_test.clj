903ff2a77e9771aa53defa18484d85b14fbb3134
J Patrick Davenport <patrick.davenport@deusdatsolutions.com>
2015-04-17 23:40:14 -0400

    Adds test corrections

--- test/waller/core_test.clj
+++ test/waller/core_test.clj
@@ -3,28 +3,33 @@
             [waller.core :refer :all]
             [ragtime.core :as rag]))
 
-(def ^:private open-no-credentials "http://localhost:8529")
-(def ^:private open-credentials "http://tester:pass1@localhost:8529")
-(def ^:private secure-no-credentials "https://localhost:8529")
-(def ^:private secure-credentials "https://tester2:pass2@localhost:8529")
+(def ^:private open-no-credentials "arango://localhost:8529/test-db")
+(def ^:private open-credentials "arango://tester:pass1@localhost:8529/test-db")
+(def ^:private secure-no-credentials "arangos://localhost:8529/test-db")
+(def ^:private secure-credentials 
+  "arangos://tester2:pass2@localhost:8529/test-db")
 
 
 (deftest connection-creation
   (testing 
     "Should create a connection for the proper http url"
-    (is (= (->ArangoDatabase {:type :simple, :url "http://localhost:8529"})
+    (is (= (->ArangoDatabase {:type :simple, :url "http://localhost:8529"} 
+             "test-db")
            (rag/connection open-no-credentials))))
   (testing 
     "Should create a connection for the proper https url"
-    (is (= (->ArangoDatabase {:type :simple, :url "https://localhost:8529"})
+    (is (= (->ArangoDatabase {:type :simple, :url "https://localhost:8529"}
+             "test-db")
            (rag/connection secure-no-credentials))))
   (testing 
     "Should create a connection for the proper http url with credentials"
     (is (= (->ArangoDatabase {:type :simple, :url "http://localhost:8529",
-                              :username "tester", :password "pass1"})
+                              :username "tester", :password "pass1"}
+             "test-db")
            (rag/connection open-credentials))))
   (testing 
     "Should create a connection for the proper https url with credentials"
     (is (= (->ArangoDatabase {:type :simple, :url "https://localhost:8529",
-                              :username "tester2", :password "pass2"})
+                              :username "tester2", :password "pass2"}
+             "test-db")
            (rag/connection secure-credentials)))))
