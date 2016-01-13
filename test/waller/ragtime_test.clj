75e7b0133a62608c6780516e3c7c725a1611fe2f
J Patrick Davenport <patrick.davenport@deusdatsolutions.com>
2016-01-12 21:15:03 -0500

    Corrects failing test for this run

--- test/waller/ragtime_test.clj
+++ test/waller/ragtime_test.clj
@@ -2,10 +2,11 @@
   (:require [clojure.test :refer :all]
             [travesedo.collection :as col]
             [travesedo.index :as tidx]
+            [travesedo.database :as db]
             [waller.ragtime :refer :all]))
 
 (def ctx {:conn {:type :simple,
-                 :url "http://arangodb26:8529"},
+                 :url "http://arangodb27:8529"},
           :db "waller",
           :collection "blog"})
 
@@ -19,22 +20,22 @@
 ;; Presumes that there is a folder named test-resources and that
 ;; the folder is added as a resource-paths
 (deftest creates-database
-  (let [ac (arango-connection "arangos://testing:pwd@arangodb:8529/testing-db")]
-    (is (= {:type :simple, :url "https://arangodb:8529",
+  (let [ac (arango-connection "arangos://testing:pwd@arangodb27:8529/testing-db")]
+    (is (= {:type :simple, :url "https://arangodb27:8529",
             :uname "testing", :password "pwd"} 
            (:conn ac)))
     (is (= "testing-db" (:db ac)))))
 
 (deftest loads-edns
+  (println "Loading edns")
   (let [edns (load-resources "migrations")]
     (is (= 2 (count edns)))))
 
 (deftest integration-test
   ;; Makes sure that the database is returned to normal.
-  (let [c  (col/get-all-collections (merge ctx {:exclude-system true}))]
-    (is (= 1 (count (:names c))))
-    (is (:waller (:names c))))
-  (migrate-from-classpath {:url "arango://arangodb26:8529/waller"})
+  (let [c  (db/drop ctx)]
+    (is (some #{(:code c)} '(200 404))))
+  (migrate-from-classpath {:url "arango://arangodb27:8529/waller"})
   (let [c (col/get-all-collections (merge ctx {:exclude-system true}))
         blog (:blog (:names c))
         blog-indexes (tidx/read-all ctx "blog")
